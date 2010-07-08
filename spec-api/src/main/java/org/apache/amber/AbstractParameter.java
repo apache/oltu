a410f3a8b2e8b85b092a10846ced432e2bc679e2
Simone Tripodi <simonetripodi@apache.org>
2010-07-08 19:55:08 +0000

    added OAuth request message and aux request message implementation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/amber/trunk@961909 13f79535-47bb-0310-9956-ffa450edef68

---
+++ spec-api/src/main/java/org/apache/amber/AbstractParameter.java
@@ -0,0 +1,159 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.amber;
+
+import java.util.Map.Entry;
+
+/**
+ * 
+ *
+ * @version $Id$
+ * @param <K>
+ */
+abstract class AbstractParameter<N>
+        implements Entry<N, String>, Comparable<AbstractParameter<N>> {
+
+    /**
+     * A non-zero, odd number used as the initial value.
+     */
+    private final static int INITIAL_ODD_NUMBER = 1;
+
+    /**
+     * A non-zero, odd number used as the multiplier.
+     */
+    private static final int MULTIPLIER_ODD_NUMBER = 31;
+
+    /**
+     * The parameter key.
+     */
+    private final N key;
+
+    /**
+     * The parameter value.
+     */
+    private final String value;
+
+    /**
+     * Creates a new parameter by his key and value.
+     *
+     * @param key the parameter key.
+     * @param value the parameter value.
+     */
+    public AbstractParameter(N key, String value) {
+        if (key == null) {
+            throw new IllegalArgumentException("Paramater 'key' must not be null");
+        }
+        if (value == null) {
+            throw new IllegalArgumentException("Paramater 'value' must not be null");
+        }
+        this.key = key;
+        this.value = value;
+    }
+
+    public final int compareTo(AbstractParameter<N> parameter) {
+        int nameComparison = String.valueOf(this.key).compareTo(String.valueOf(parameter.getKey()));
+        if (nameComparison == 0) {
+            return this.value.compareTo(parameter.getValue());
+        }
+        return nameComparison;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public final N getKey() {
+        return this.key;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public final String getValue() {
+        return this.value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public final String setValue(String value) {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final int hashCode() {
+        int result = INITIAL_ODD_NUMBER;
+        result = MULTIPLIER_ODD_NUMBER * result + ((this.key == null) ? 0 : this.key.hashCode());
+        result = MULTIPLIER_ODD_NUMBER * result + ((this.value == null) ? 0 : this.value.hashCode());
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        @SuppressWarnings({ "rawtypes" })
+        AbstractParameter other = (AbstractParameter) obj;
+
+        if (this.key == null) {
+            if (other.getKey() != null) {
+                return false;
+            }
+        } else if (!String.valueOf(this.key).equals(String.valueOf(other.getKey()))) {
+            return false;
+        }
+
+        if (this.value == null) {
+            if (other.getValue() != null) {
+                return false;
+            }
+        } else if (!this.value.equals(other.getValue())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return new StringBuilder("Parameter { key=")
+                .append(this.key)
+                .append(", value=")
+                .append(this.value)
+                .append(" }")
+                .toString();
+    }
+
+}
