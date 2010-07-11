cd493960e9cbdeec644f24e803057d44915580f7
Simone Tripodi <simonetripodi@apache.org>
2010-07-11 09:06:06 +0000

    added methods to load a key from classpath, file or URL
    git-svn-id: https://svn.apache.org/repos/asf/incubator/amber/trunk@963019 13f79535-47bb-0310-9956-ffa450edef68

--- signature-api/src/main/java/org/apache/amber/signature/rsa/AbstractRsaSha1Key.java
+++ signature-api/src/main/java/org/apache/amber/signature/rsa/AbstractRsaSha1Key.java
@@ -16,9 +16,11 @@
  */
 package org.apache.amber.signature.rsa;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -36,15 +38,74 @@ abstract class AbstractRsaSha1Key implements Key {
 
     private final byte[] byteValue;
 
+    /**
+     * Loads a key from a certificate located in the classpath.
+     *
+     * @param certificateClasspathLocation
+     * @throws SignatureException
+     */
     public AbstractRsaSha1Key(String certificateClasspathLocation) throws SignatureException {
-        this(Thread.currentThread().getContextClassLoader().getResource(certificateClasspathLocation));
+        if (certificateClasspathLocation == null) {
+            throw new SignatureException("parameter 'certificateClasspathLocation' must not be null");
+        }
+        if ('/' == certificateClasspathLocation.charAt(0)) {
+            certificateClasspathLocation = certificateClasspathLocation.substring(1);
+        }
+
+        URL certificateURL = this.getClass().getClassLoader().getResource(certificateClasspathLocation);
+        if (certificateURL == null) {
+            certificateURL = Thread.currentThread().getContextClassLoader().getResource(certificateClasspathLocation);
+        }
+
+        if (certificateURL == null) {
+            throw new SignatureException("Classpath resource '"
+                    + certificateClasspathLocation
+                    + "' not found, please make sure it exists in the classpath");
+        }
+
+        this.byteValue = this.readCertificate(certificateURL);
     }
 
+    /**
+     * Loads a key from a certificate located in a file.
+     *
+     * @param certificateFileLocation
+     * @throws SignatureException
+     */
+    public AbstractRsaSha1Key(File certificateFileLocation) throws SignatureException {
+        if (certificateFileLocation == null) {
+            throw new SignatureException("parameter 'certificateFileLocation' must not be null");
+        }
+        if (certificateFileLocation.exists() || certificateFileLocation.isDirectory()) {
+            throw new SignatureException("Impossible to load the certificate, '"
+                    + certificateFileLocation
+                    + "' doesn't exist or is a directory");
+        }
+
+        try {
+            this.byteValue = this.readCertificate(certificateFileLocation.toURI().toURL());
+        } catch (MalformedURLException e) {
+            throw new SignatureException("Impossible to read the certificate from '"
+                    + certificateFileLocation
+                    + "' file", e);
+        }
+    }
+
+    /**
+     * Loads a key from a certificate located in an URL.
+     *
+     * @param certificateURL
+     * @throws SignatureException
+     */
     public AbstractRsaSha1Key(URL certificateURL) throws SignatureException {
         if (certificateURL == null) {
             throw new SignatureException("parameter 'certificateURL' must not be null");
         }
 
+        this.byteValue = this.readCertificate(certificateURL);
+    }
+
+    private byte[] readCertificate(URL certificateURL) throws SignatureException {
         URLConnection urlConnection = null;
         InputStream input = null;
 
@@ -52,7 +113,7 @@ abstract class AbstractRsaSha1Key implements Key {
             urlConnection = certificateURL.openConnection();
             input = urlConnection.getInputStream();
 
-            this.byteValue = this.readCertificate(input);
+            return this.readCertificate(input);
         } catch (Exception e) {
             throw new SignatureException("Impossible to read the certificate from '"
                     + certificateURL
