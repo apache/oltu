ebec60824026cfb24c9fde7c68aa1bb4d702afb4
Simone Tripodi <simonetripodi@apache.org>
2010-07-11 14:55:18 +0000

    first checkin of RsaSha1MethodTestCase class
    git-svn-id: https://svn.apache.org/repos/asf/incubator/amber/trunk@963089 13f79535-47bb-0310-9956-ffa450edef68

---
+++ signature-api/src/test/java/org/apache/amber/signature/rsa/RsaSha1MethodTestCase.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.amber.signature.rsa;
+
+import org.apache.amber.signature.AbstractSimpleMethodTestCase;
+
+/**
+ * 
+ *
+ * @version $Id$
+ */
+public final class RsaSha1MethodTestCase extends AbstractSimpleMethodTestCase {
+
+    // TODO test disabled, expected signature has to be calculated
+    public void verifyRsaSha1MethodAlgorithm() throws Exception {
+        this.verifySignature(new PemRsaSha1VerifyingKey("kd94hf93k423kf44"),
+                new RsaSha1Method(),
+                "kd94hf93k423kf44&pfkkdhi9sl3r4s00");
+    }
+
+}
