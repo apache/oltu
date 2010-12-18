23fcd5e92c15cebfa01943de7ad50190cba33fea
Tommaso Teofili <tommaso@apache.org>
2010-12-18 16:14:04 +0000

    [AMBER-11] - import of Leelo OAuth 2.0 implementation - oauth2-client module
    git-svn-id: https://svn.apache.org/repos/asf/incubator/amber/trunk@1050659 13f79535-47bb-0310-9956-ffa450edef68

---
+++ oauth-2.0/oauth2-client/src/main/java/org/apache/amber/oauth2/client/OAuthClient.java
@@ -0,0 +1,66 @@
+/**
+ *       Copyright 2010 Newcastle University
+ *
+ *          http://research.ncl.ac.uk/smart/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.amber.oauth2.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.amber.oauth2.client.request.OAuthClientRequest;
+import org.apache.amber.oauth2.client.response.OAuthAccessTokenResponse;
+import org.apache.amber.oauth2.client.response.OAuthJSONAccessTokenResponse;
+import org.apache.amber.oauth2.common.exception.OAuthProblemException;
+import org.apache.amber.oauth2.common.exception.OAuthSystemException;
+import org.apache.amber.oauth2.common.OAuth;
+
+/**
+ * OAuth Client - exposes a high-level API for Client Applications
+ *
+ * @author Maciej Machulak (m.p.machulak@ncl.ac.uk)
+ * @author Lukasz Moren (lukasz.moren@ncl.ac.uk)
+ * @author Aad van Moorsel (aad.vanmoorsel@ncl.ac.uk)
+ */
+public class OAuthClient {
+
+    protected HttpClient httpClient;
+
+    public OAuthClient(HttpClient oauthClient) {
+        this.httpClient = oauthClient;
+    }
+
+    public <T extends OAuthAccessTokenResponse> T accessToken(
+        OAuthClientRequest request,
+        Class<T> responseClass)
+        throws OAuthSystemException, OAuthProblemException {
+
+        String method = OAuth.HttpMethod.POST;
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(OAuth.HeaderType.CONTENT_TYPE, OAuth.ContentType.URL_ENCODED);
+
+        return httpClient.execute(request, headers, method, responseClass);
+    }
+
+    public OAuthJSONAccessTokenResponse accessToken(
+        OAuthClientRequest request)
+        throws OAuthSystemException, OAuthProblemException {
+        return accessToken(request, OAuthJSONAccessTokenResponse.class);
+    }
+}
---
+++ oauth-2.0/oauth2-client/src/test/java/org/apache/amber/oauth2/client/validator/TokenValidatorTest.java
@@ -0,0 +1,50 @@
+/**
+ *       Copyright 2010 Newcastle University
+ *
+ *          http://research.ncl.ac.uk/smart/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.amber.oauth2.client.validator;
+
+import org.apache.amber.oauth2.client.validator.OAuthClientValidator;
+import org.apache.amber.oauth2.client.validator.TokenValidator;
+import org.junit.Assert;
+import org.junit.Test;
+import org.apache.amber.oauth2.common.OAuth;
+
+
+/**
+ * @author Maciej Machulak (m.p.machulak@ncl.ac.uk)
+ * @author Lukasz Moren (lukasz.moren@ncl.ac.uk)
+ * @author Aad van Moorsel (aad.vanmoorsel@ncl.ac.uk)
+ */
+public class TokenValidatorTest {
+
+    @Test
+    public void testValidator() {
+
+        OAuthClientValidator validator = new TokenValidator();
+        Assert.assertNotNull(validator.requiredParams);
+        Assert.assertEquals(1, validator.requiredParams.size());
+        Assert.assertArrayEquals(new String[] {}, validator.requiredParams.get(OAuth.OAUTH_ACCESS_TOKEN));
+        Assert.assertNotNull(validator.notAllowedParams);
+        Assert.assertEquals(1, validator.notAllowedParams.size());
+        Assert.assertTrue(validator.notAllowedParams.contains(OAuth.OAUTH_CODE));
+    }
+
+}
