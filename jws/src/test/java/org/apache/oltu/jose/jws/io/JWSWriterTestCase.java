dab022313ae96d17877f151ecd82fb35c09b2658
Antonio Sanso <asanso@apache.org>
2013-12-20 15:48:21 +0000

    OLTU-131 - Signature parsing from access token is broken for character encodings that are not single-byte
    *  apply patch from Andrei Dulvac (thanks !!)
    
    git-svn-id: https://svn.apache.org/repos/asf/oltu/trunk/jose@1552707 13f79535-47bb-0310-9956-ffa450edef68

--- jws/src/test/java/org/apache/oltu/jose/jws/io/JWSWriterTestCase.java
+++ jws/src/test/java/org/apache/oltu/jose/jws/io/JWSWriterTestCase.java
@@ -32,9 +32,9 @@ public final class JWSWriterTestCase {
                          .setType("JWT")
                          .setCritical(new String[]{ "exp" })
                          .setPayload("{\"iss\":\"joe\",\r\n \"exp\":1300819380,\r\n \"http://example.com/is_root\":true}")
-                         .setSignature("tߴ���%O�`}ح���%Miֿ�[XXS��y")
+                         .setSignature("dBjftO-_ve-_ve-_vSVP77-9YH3Yre-_ve-_vRbvv70lTWnWv--_vVtYBVhT77-977-9eQ")
                          .build();
-        String actual = jwsWriter.write(jws).replaceAll("\r?\n", "");
+        String actual = jwsWriter.write(jws);
         assertEquals("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImNyaXQiOlsiZXhwIl19.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftO-_ve-_ve-_vSVP77-9YH3Yre-_ve-_vRbvv70lTWnWv--_vVtYBVhT77-977-9eQ",
                      actual);
     }
