6d45af9831a6282ed7f57074424029a570067952
Antonio Sanso <asanso@apache.org>
2014-04-01 10:56:28 +0000

    OLTU-143 - Add support for public key signature algorithm to the JWS module
    git-svn-id: https://svn.apache.org/repos/asf/oltu/trunk/jose@1583609 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jws/src/main/java/org/apache/oltu/jose/jws/signature/impl/SignatureMethodRSAImpl.java
@@ -0,0 +1,118 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.oltu.jose.jws.signature.impl;
+ 
+import java.security.Signature;
+import org.apache.oltu.commons.encodedtoken.TokenDecoder;
+import org.apache.oltu.jose.jws.JwsConstants;
+import org.apache.oltu.jose.jws.signature.SignatureMethod;
+
+/**
+ * Class that asymmetrically sign and verify the
+ * issued token 
+ * 
+ */
+public class SignatureMethodRSAImpl implements
+SignatureMethod<PrivateKey, PublicKey>{
+    
+    private String algorithm;
+
+    public SignatureMethodRSAImpl(String algorithm) {
+        super();
+        this.algorithm = algorithm;
+    }
+
+
+    /*
+     * Calculate the signature of given header.payload as for 
+     * http://tools.ietf.org/html/draft-ietf-jose-json-web-signature-21#appendix-A.2.1
+     *  
+     */
+    @Override
+    public String calculate(String header, String payload, PrivateKey signingKey) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(header).append(".").append(payload);
+        final String stringToSign = sb.toString();
+        byte[] bytes = stringToSign.getBytes();
+        try {
+            Signature signature = Signature.getInstance(getAlgorithmInternal());
+            
+            signature.initSign(signingKey.getPrivateKey());
+            signature.update(bytes);
+            bytes = signature.sign();
+            
+            return TokenDecoder.base64Encode(bytes);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }  
+    }
+
+    /*
+     * Verify the signature of given header.payload as for 
+     * http://tools.ietf.org/html/draft-ietf-jose-json-web-signature-21#appendix-A.2.2
+     *  
+     */
+    @Override
+    public boolean verify(String signature, String header, String payload,
+            PublicKey verifyingKey) {
+        final String text = header + "." + payload;
+        try {
+            Signature sign = Signature.getInstance(getAlgorithmInternal());
+            sign.initVerify(verifyingKey.getPublicKey());
+            sign.update(text.getBytes());
+ 
+            return sign.verify(decode(signature));
+             
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Override
+    public String getAlgorithm() {
+        return algorithm;
+    }
+    
+    // ---------- Private methods ---------------------------------------------
+    private String getAlgorithmInternal(){
+        String alg = null;
+        if (JwsConstants.RS256.equals(algorithm)) { 
+            alg = "SHA256withRSA"; 
+        } else if (JwsConstants.RS384.equals(algorithm)) { 
+            alg = "SHA384withRSA"; 
+        } else if (JwsConstants.RS512.equals(algorithm)) { 
+            alg = "SHA512withRSA"; 
+        }
+        return alg;
+    }
+    
+    private static byte[] decode(String arg) throws Exception {
+        String s = arg;
+        s = s.replace('-', '+'); // 62nd char of encoding
+        s = s.replace('_', '/'); // 63rd char of encoding
+        switch (s.length() % 4) // Pad with trailing '='s
+        {
+          case 0: break; // No pad chars in this case
+          case 2: s += "=="; break; // Two pad chars
+          case 3: s += "="; break; // One pad char
+          default: throw new java.lang.Exception(
+            "Illegal base64url string!");
+        }
+        return TokenDecoder.base64DecodeToByte(s);
+      }
+
+}
