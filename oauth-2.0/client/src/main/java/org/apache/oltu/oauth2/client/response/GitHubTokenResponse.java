59ee35337f3957748878c34650ecae0ab4593460
Stein Welberg <stein@apache.org>
2016-03-01 05:45:45 +0000

    OLTU-173 Fix GithubTokenRespons. Thanks Gualtiero Testa for the patch!
    git-svn-id: https://svn.apache.org/repos/asf/oltu/trunk@1732996 13f79535-47bb-0310-9956-ffa450edef68

--- oauth-2.0/client/src/main/java/org/apache/oltu/oauth2/client/response/GitHubTokenResponse.java
+++ oauth-2.0/client/src/main/java/org/apache/oltu/oauth2/client/response/GitHubTokenResponse.java
@@ -27,47 +27,53 @@ import org.apache.oltu.oauth2.common.token.OAuthToken;
 import org.apache.oltu.oauth2.common.utils.OAuthUtils;
 
 /**
- *
- *
- *
+ * Custom response class
  */
 public class GitHubTokenResponse extends OAuthAccessTokenResponse {
 
+    @Override
     public String getAccessToken() {
         return getParam(OAuth.OAUTH_ACCESS_TOKEN);
     }
 
     @Override
     public String getTokenType() {
-    	return getParam(OAuth.OAUTH_TOKEN_TYPE);
+        return getParam(OAuth.OAUTH_TOKEN_TYPE);
     }
 
+    @Override
     public Long getExpiresIn() {
         String value = getParam(OAuth.OAUTH_EXPIRES_IN);
         return value == null? null: Long.valueOf(value);
     }
 
+    @Override
     public String getRefreshToken() {
-        return getParam(OAuth.OAUTH_EXPIRES_IN);
+        return getParam(OAuth.OAUTH_REFRESH_TOKEN);
     }
 
+    @Override
     public String getScope() {
         return getParam(OAuth.OAUTH_SCOPE);
     }
 
+    @Override
     public OAuthToken getOAuthToken() {
         return new BasicOAuthToken(getAccessToken(), getTokenType(), getExpiresIn(), getRefreshToken(), getScope());
     }
 
+    @Override
     protected void setBody(String body) {
         this.body = body;
         parameters = OAuthUtils.decodeForm(body);
     }
 
+    @Override
     protected void setContentType(String contentType) {
         this.contentType = contentType;
     }
 
+    @Override
     protected void setResponseCode(int code) {
         this.responseCode = code;
     }
