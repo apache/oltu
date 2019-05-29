8e6564e04556c907b1396a2fd1747824150ced5a
shahajipatil22 <33981606+shahajipatil22@users.noreply.github.com>
2019-05-29 12:50:51 +0530

    Update AuthzController.java

--- demos/client-demo/src/main/java/org/apache/oltu/oauth2/client/demo/controller/AuthzController.java
+++ demos/client-demo/src/main/java/org/apache/oltu/oauth2/client/demo/controller/AuthzController.java
@@ -40,6 +40,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Handles requests for the application welcome page.
  */
