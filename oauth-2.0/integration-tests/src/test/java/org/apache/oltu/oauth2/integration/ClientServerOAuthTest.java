dd3d6e1fae4c828529084b12bf27b286ab1654f4
digithuman <cgd321@gmail.com>
2023-01-02 12:04:45 +0800

    spring-test cxf 已创建服务

--- oauth-2.0/integration-tests/src/test/java/org/apache/oltu/oauth2/integration/ClientServerOAuthTest.java
+++ oauth-2.0/integration-tests/src/test/java/org/apache/oltu/oauth2/integration/ClientServerOAuthTest.java
@@ -38,23 +38,23 @@ public abstract class ClientServerOAuthTest extends JUnit4SpringContextTests {
     @BeforeClass
     public static void initService() throws Exception {
 
-        JAXRSServerFactoryBean sf = (JAXRSServerFactoryBean)ctx.getBean("oauthServer");
-        s = sf.create();
-
-        JAXRSServerFactoryBean sf2 = (JAXRSServerFactoryBean)ctx.getBean("oauthClient");
-        s2 = sf2.create();
+//        JAXRSServerFactoryBean sf = (JAXRSServerFactoryBean)ctx.getBean("oauthServer");
+//        s = sf.create();
+//
+//        JAXRSServerFactoryBean sf2 = (JAXRSServerFactoryBean)ctx.getBean("oauthClient");
+//        s2 = sf2.create();
 
 
     }
 
     @AfterClass
     public static void stopService() throws Exception {
-        if (s != null) {
-            s.stop();
-        }
-        if (s2 != null) {
-            s2.stop();
-        }
+//        if (s != null) {
+//            s.stop();
+//        }
+//        if (s2 != null) {
+//            s2.stop();
+//        }
     }
 
 
